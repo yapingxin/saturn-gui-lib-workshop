a0730bce4ec8c478121e21181559f072800b83f9
yapingxin <xinyp@live.com>
2016-07-18 13:53:52 +0800

    [FTGL] multilingual display

--- Workshop/BestPractice/FreeTypePractice/FT_gl_demo/FT_gl_demo.cpp
+++ Workshop/BestPractice/FreeTypePractice/FT_gl_demo/FT_gl_demo.cpp
@@ -1,4 +1,4 @@
-// FT_gl_demo.cpp : Defines the entry point for the console application.
+﻿// FT_gl_demo.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
@@ -21,6 +21,7 @@ static void keyboard(unsigned char key, int x, int y);
 static void display(void);
 
 FTGLPixmapFont *freeTypeFont = NULL;
+wchar_t str[128] = L"FTGL 中文输出测试";
 
 void init(void)
 {
@@ -31,18 +32,58 @@ void init(void)
 
 	// Initialize FTGL
 	freeTypeFont = new FTGLPixmapFont(FontLibFilePath);
+	if (freeTypeFont == NULL)
+	{
+		printf("%s", "Font lib data file cannot be loaded.");
+		exit(0);
+	}
+	else
+	{
+		freeTypeFont->FaceSize(24); // <-- !!!
+	}
+}
 
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 10.0f);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+	glRasterPos2f(0.2, 0.5);
+	freeTypeFont->Render(str);
+
+	glutSwapBuffers();
+}
+
+void reshape(int w, int h)
+{
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, (GLdouble)w / (GLdouble)h, 1.0, 10.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glutPostRedisplay();
 }
 
 
-int main(int argc, char* argv[])
+int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
-	glutCreateWindow("GLUT Test");
-	glutKeyboardFunc(&keyboard);
-	glutDisplayFunc(&display);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(600, 600);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("FreeType multilingual display");
+	init();
+	glutReshapeFunc(reshape);
+	glutDisplayFunc(display);
 	glutMainLoop();
-
 	return 0;
 }
 
@@ -57,20 +98,6 @@ static void keyboard(unsigned char key, int x, int y)
 }
 
 
-static void display()
-{
-	glClear(GL_COLOR_BUFFER_BIT);
 
-	glColor3f(1.0f, 0.0f, 0.0f);
-
-	glBegin(GL_POLYGON);
-	glVertex2f(-0.5f, -0.5f);
-	glVertex2f(0.5f, -0.5f);
-	glVertex2f(0.5f, 0.5f);
-	glVertex2f(-0.5f, 0.5f);
-	glEnd();
-
-	glFlush();
-}
 
 
